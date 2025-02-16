36ce9e211c2ee28d74102cfef141c2129fe3f24d
priyadharshinitc <priyadharshinihp@gmail.com>
2025-02-16 13:05:29 +0530

    Navbar made responsive

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        // screens: {
+        //   "max-md": { max: "50rem" },    // max-width: 800px
+        //   "max-xs": { max: "31.25rem" }, // max-width: 500px
+        // },
+      },
     },
     plugins: [],
 }
