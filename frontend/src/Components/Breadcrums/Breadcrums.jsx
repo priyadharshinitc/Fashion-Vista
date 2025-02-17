b228245a395f132cfc19efafc24ce7e7ba2a3095
priyadharshinitc <priyadharshinihp@gmail.com>
2025-02-17 16:21:01 +0530

    Margin added for 1440px

--- frontend/src/Components/Breadcrums/Breadcrums.jsx
+++ frontend/src/Components/Breadcrums/Breadcrums.jsx
@@ -4,7 +4,7 @@ import arrow_icon from '../Assets/breadcrum_arrow.png'
 const Breadcrums = (props) => {
     const {product} = props
   return (
-    <div className='breadcrum flex items-center gap-2 text-[#5E5E5E] text-base font-semibold my-[60px] max-xl:my-8 max-xl:mx-12 max-lg:mx-8 max-md:mx-2'>
+    <div className='breadcrum flex items-center gap-2 text-[#5E5E5E] text-base font-semibold my-[60px] mx-[20px] max-xl:my-8 max-xl:mx-12 max-lg:mx-8 max-md:mx-2'>
         Home <img src={arrow_icon} alt="Arrow Icon" /> Shop <img src={arrow_icon} alt="Arrow Icon" /> {product.category} <img src={arrow_icon} alt="Arrow Icon" /> {product.name}
     </div>
   )
