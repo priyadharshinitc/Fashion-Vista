d3e02ca74a3356b52c70d21779c3471235af57d8
priyadharshinitc <priyadharshinihp@gmail.com>
2025-02-17 12:38:14 +0530

    Mediaqueries added for LoginSignup page

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -11,16 +11,16 @@ const LoginSignup = () => {
     fontSize: "16px"
   }
   return (
-    <div className='loginsignup w-full h-[83vh] bg-[#FCE3FE] pt-[50px] pb-[50px] mb-[30px]'>
-      <div className="loginsignup-container w-[580px] h-fit bg-white py-10 px-12 m-auto">
-        <h1 className="text-3xl font-semibold text-[#171717] my-1 mx-0">Sign Up</h1>
-        <div className="loginsignup-fields flex flex-col gap-7 mt-[30px]">
-          <input type="text" placeholder='Your Name' style={loginsignupFieldsInputStyles} />
+    <div className='loginsignup w-full h-[93vh] bg-[#FCE3FE] pt-[50px] pb-[50px] mb-[30px] max-xl:pt-[50px]'>
+      <div className="loginsignup-container w-[580px] h-fit bg-white py-5 px-12 m-auto max-xl:w-auto max-xl:max-w-[500px] max-xl:h-[500px] max-sm:w-[80%] max-sm:p-4">
+        <h1 className="text-3xl font-semibold text-[#171717] my-1 mx-0 max-xl:my-2 max-xl:mx-auto max-md:text-2xl">Sign Up</h1>
+        <div className="loginsignup-fields flex flex-col gap-7">
+          <input type="text" placeholder='Your Name' style={loginsignupFieldsInputStyles} className='max-xl:gap-5 max-xl:mt-5' />
           <input type="email" placeholder='Email Address' style={loginsignupFieldsInputStyles} />
           <input type="password" placeholder='Password' style={loginsignupFieldsInputStyles} />
         </div>
-        <button className='w-[100%] h-12 text-white bg-[#FF4141] border-none text-xl font-medium cursor-pointer mt-[30px]'>Continue</button>
-        <p className='loginsignup-login text-[#5C5C5C] text-xl font-medium mt-5' >Already have an account? <span className='text-[#FF4141] font-semibold'>Login</span></p>
+        <button className='w-[100%] h-12 text-white bg-[#FF4141] border-none text-xl font-medium cursor-pointer mt-[30px] max-md:text-base'>Continue</button>
+        <p className='loginsignup-login text-[#5C5C5C] text-xl font-medium mt-5 max-xl:text-base' >Already have an account? <span className='text-[#FF4141] font-semibold'>Login</span></p>
       </div>
     </div>
   )
