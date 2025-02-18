1ee42c9a2754ce8b8f6d002f5b3d4ba2e8f82c81
priyadharshinitc <priyadharshinihp@gmail.com>
2025-02-18 18:08:11 +0530

    Admin Sidebar created

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -5,17 +5,17 @@ import checklist from '../../assets/checklist.png'
 
 const Sidebar = () => {
   return (
-    <div className='sidebar flex flex-col bg-gray-200 pt-8 gap-5 w-full max-w-[250px] h-screen'>
+    <div className='sidebar flex flex-col bg-gray-300 pt-8 gap-5 w-full max-w-[250px] h-screen max-md:py-8 max-md:px-0 max-md:flex-row max-md:w-full max-md:max-w-none max-md:h-auto max-md:justify-center'>
         <Link to={"/addproduct"} className='no-underline'>
-            <div className="sidebar-item">
-                <img src={cartIcon} alt="Cart Icon" className='w-10 h-10' />
+            <div className="sidebar-item flex justify-center items-center my-0 mx-5 py-2 px-3 rounded-sm bg-[#F6F6F6] gap-5 cursor-pointer max-sm:p-2 max-sm:gap-2">
+                <img src={cartIcon} alt="Cart Icon" className='w-5 h-5' />
                 <p>Add Product</p>
             </div>
         </Link>
 
         <Link to={"/listproduct"} className='no-underline'>
-            <div className="sidebar-item">
-                <img src={checklist} alt="Checklist" className='w-10 h-10' />
+            <div className="sidebar-item flex justify-center items-center my-0 mx-5 py-2 px-3 rounded-sm bg-[#F6F6F6] gap-5 cursor-pointer max-sm:p-2 max-sm:gap-2">
+                <img src={checklist} alt="Checklist" className='w-5 h-5' />
                 <p>List Product</p>
             </div>
         </Link>
