96caba9c49979e1d0074d3698dcc707c8880d70f
priyadharshinitc <priyadharshinihp@gmail.com>
2025-02-18 23:46:10 +0530

    Started working in ListProduct

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const ListProduct = () => {
   return (
-    <div className='list-product'>ListProduct</div>
+    <div className='list-product'>
+      <h1 className='text-2xl font-bold'>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+    </div>
   )
 }
 
