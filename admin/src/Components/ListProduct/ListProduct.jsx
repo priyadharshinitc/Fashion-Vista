e2a8e4d8083209894248f575305d8d6caf99b841
priyadharshinitc <priyadharshinihp@gmail.com>
2025-02-18 19:02:58 +0530

    Add Product page created

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListProduct = () => {
+  return (
+    <div className='list-product'>ListProduct</div>
+  )
+}
+
+export default ListProduct
--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
-
+import { Routes, Route } from 'react-router-dom'
+import AddProduct from '../../Components/AddProduct/AddProduct'
+import ListProduct from '../../Components/ListProduct/ListProduct'
 const Admin = () => {
   return (
-    <div className='admin'>
+    <div className='admin flex max-md:flex-col'>
         <Sidebar />
+        <Routes>
+          <Route path="/addproduct" element={<AddProduct />}></Route>
+          <Route path="/listproduct" element={<ListProduct />}></Route>
+        </Routes>
     </div>
   )
 }
