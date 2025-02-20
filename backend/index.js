6893160a164043967b515e3b101b904fc6120d18
priyadharshinitc <priyadharshinihp@gmail.com>
2025-02-20 08:33:45 +0530

    APIs added for Signup and Login

--- backend/index.js
+++ backend/index.js
@@ -158,7 +158,46 @@ app.post("/signup", async (req, res) => {
 
     let cart = {}
     for (let index = 0; index < 300; index++) {
-        cart[i] = 0        
+        cart[index] = 0        
+    }
+
+    const user = new Users({
+        name: req.body.user,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart
+    })
+
+    await user.save()
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, "secret_ecom")
+    res.json({success: true, token})
+})
+
+// Creating End Point for User Login
+app.post("/login", async (req, res) => {
+    let user = await Users.findOne({email: req.body.email})
+    if(user) {
+        const passCompare = (req.body.password === user.password)
+        if(passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data, "secret_ecom")
+            res.json({success: true, token})
+        } else {
+            res.json({success: false, error: "Wrong Password"})
+        }
+    } else {
+        res.json({success: false, error: "Wrong Email Address"})
     }
 })
 
