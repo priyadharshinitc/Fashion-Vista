74f5c313683f0b882deeee59e83a920bff38e465
priyadharshinitc <priyadharshinihp@gmail.com>
2025-02-20 13:57:28 +0530

    APIs created for Login and Signup

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,26 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginSignup = () => {
   const loginsignupFieldsInputStyles = {
     height: "60px",
     width: "100%",
     paddingLeft: "20px",
+    marginTop: "20px",
     border: "1px solid #C9C9C9",
     outline: "none",
     color: "#5C5C5C",
     fontSize: "16px"
   }
+
+  const [state, setState] = useState("Login")
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: ""
+  })
+
+  const changeHandler = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const signup = async () => {
+    console.log("Sign Up Function Executed", formData)
+
+    let response = await fetch("http://localhost:5000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+
+    let responseData = await response.json()
+    
+    if(responseData.success) {
+      localStorage.setItem("auth-token", responseData.token)
+      window.location.replace("/")
+    } else {
+      alert(responseData.error)
+    }
+  }
+
+  const login = async () => {
+    console.log("Login Function Executed", formData)
+
+    let response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+
+    let responseData = await response.json()
+    
+    if(responseData.success) {
+      localStorage.setItem("auth-token", responseData.token)
+      window.location.replace("/")
+    } else {
+      alert(responseData.error)
+    }
+  }
+
   return (
     <div className='loginsignup w-full h-fit bg-[#FCE3FE] pt-[50px] pb-[50px] mb-[30px] max-xl:pt-[50px]'>
       <div className="loginsignup-container w-[580px] h-fit bg-white py-5 px-12 m-auto max-xl:w-auto max-xl:max-w-[500px] max-xl:h-[500px] max-sm:w-[80%] max-sm:p-4">
-        <h1 className="text-3xl font-semibold text-[#171717] my-1 mx-0 max-xl:my-2 max-xl:mx-auto max-md:text-2xl">Sign Up</h1>
+        <h1 className="text-3xl font-semibold text-[#171717] my-1 mx-0 max-xl:my-2 max-xl:mx-auto max-md:text-2xl">{state}</h1>
         <div className="loginsignup-fields flex flex-col gap-7">
-          <input type="text" placeholder='Your Name' style={loginsignupFieldsInputStyles} className='max-xl:gap-5 mt-5' />
-          <input type="email" placeholder='Email Address' style={loginsignupFieldsInputStyles} />
-          <input type="password" placeholder='Password' style={loginsignupFieldsInputStyles} />
+          {state === "Sign Up" && <input type="text" placeholder='Your Name' style={loginsignupFieldsInputStyles} className='max-xl:gap-5' name="username" value={formData.username} onChange={changeHandler} />}
+          <input type="email" placeholder='Email Address'  style={loginsignupFieldsInputStyles} name="email" value={formData.email} onChange={changeHandler} />
+          <input type="password" placeholder='Password' style={loginsignupFieldsInputStyles} name="password" value={formData.password} onChange={changeHandler} />
         </div>
-        <button className='w-[100%] h-12 text-white bg-[#FF4141] border-none text-xl font-medium cursor-pointer mt-[30px] max-md:text-base'>Continue</button>
-        <p className='loginsignup-login text-[#5C5C5C] text-xl font-medium mt-5 max-xl:text-base' >Already have an account? <span className='text-[#FF4141] font-semibold'>Login</span></p>
+        <button className='w-[100%] h-12 text-white bg-[#FF4141] border-none text-xl font-medium cursor-pointer mt-[30px] max-md:text-base' onClick={() => {(state === "Sign Up") ? signup() : login()}}>Continue</button>
+        {state === "Sign Up" && <p className='loginsignup-login text-[#5C5C5C] text-xl font-medium mt-5 max-xl:text-base'>Already have an account? <span className='text-[#FF4141] font-semibold cursor-pointer' onClick={() => setState("Login")}>Login</span></p>}
+        {state === "Login" && <p className='loginsignup-login text-[#5C5C5C] text-xl font-medium mt-5 max-xl:text-base'>Don't have an account? <span className='text-[#FF4141] font-semibold cursor-pointer' onClick={() => setState("Sign Up")}>Sign Up</span></p>}
       </div>
     </div>
   )
